b6e55b11c9801118922e2634af4a1a3f7537b2f2
pedrost <pee.dro_fs@hotmail.com>
2020-05-24 17:52:06 -0400

    [UPDATE] Parking time method round abds

--- app/models/Parking.rb
+++ app/models/Parking.rb
@@ -1,7 +1,7 @@
 class Parking < ApplicationRecord
 
   def time
-    (created_at - Time.zone.now) / 1.minutes
+    "#{( (created_at - Time.zone.now) / 1.minutes ).abs.round(0) } #{I18n.t("tz.minutes")}"
   end
 
 end
