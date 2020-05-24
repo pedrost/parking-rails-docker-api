a900f78ba316ba592c1cd9148a89dd230b987858
pedrost <pee.dro_fs@hotmail.com>
2020-05-24 17:10:06 -0400

    [CREATE] API Controller and V1 Parking Controller

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,16 @@
+class Api::ApiController < ActionController::API
+  include ApiCommonResponses
+
+  respond_to :json
+
+  protected
+
+  def serialize_resource(resource, serializer, scope: nil)
+    JSON.parse(serializer.new(resource, scope: scope).to_json)
+  end
+
+  def serialize_resource_list(resources, serializer)
+    JSON.parse(ActiveModelSerializers::SerializableResource.new(resources, each_serializer: serializer).to_json)
+  end
+
+end
---
+++ app/controllers/api/v1/parking_controller.rb
@@ -0,0 +1,18 @@
+class Api::V1::ParkingController < Api::ApiController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def out
+  end
+
+  def pay
+  end
+
+end
