8c82e45f6d34c842e3811c2424ea8168c7b36b25
pedrost <pee.dro_fs@hotmail.com>
2020-05-24 17:07:45 -0400

    [UPDATE] Config Application set default locale

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    I18n.available_locales = ['pt-BR', :en]
+    config.i18n.default_locale = :'pt-BR'
+    config.api_only = true
   end
 end
